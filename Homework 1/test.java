71b34631d1f4dbb8548041d18abfe92831937a65
seanbruno1 <162635726+seanbruno1@users.noreply.github.com>
2024-09-11 14:40:30 -0400

    Clas work

---
+++ Homework 1/test.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+//Class of 9/10// 
+
+public class test {
+    public static void main(String[] args){
+        int num1 = 0;
+        if (num1 > 1){
+            System.out.println("Yes");
+        } else{
+            System.out.println("Number is less than 1");
+        }
+
+        String myName = "Sean";
+        if (myName == "sean"){
+            System.out.println("Your name does not match");
+        } else if (myName == "Sean"){
+            System.out.println("Your name matches!");
+        } else{
+            System.out.println("Wrong");
+        }
+
+        int num3 = 4;
+        int num2 = 3;
+        if (num3 == num2){
+            System.out.println("These two numbers are equal");
+        } else{
+            System.out.println("These two numbers are not equal");
+        }
+
+    }
+}
+    
+
