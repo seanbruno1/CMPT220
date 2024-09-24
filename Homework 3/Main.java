97e135e81b5cc41478c134c78903784d95f9d701
seanbruno1 <162635726+seanbruno1@users.noreply.github.com>
2024-09-24 13:41:33 -0400

    work done

--- Homework 3/Main.java
+++ Homework 3/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
 
         //P1: This one only prints 0-9, can you fix it so it prints 1-10?
         System.out.println("Problem 1");
@@ -13,42 +15,33 @@ public class Main {
         //P2: Ask the user for a number. Create a loop to find the factorial of it
         //(factorial = X!, X being the user input, Factorials are every digit before X multiplied together)
         System.out.println("Problem 2");
-        Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number and I will tell you the fatorial: ");
-        Integer num = sc.nextInt();
-        Integer sum5 = 1;
+        Integer userNum = sc.nextInt();
+        Integer sum = 1;
         //here's a hint
-        for (int i = 1; i <= num; i++){
-            sum5 *= i; // changed this to *= so i keeps multiplying to the previous number
+        for (int i = 1; i <= userNum; i++){
+            sum *= i; 
             
         }
 
-        System.out.println(sum5);
+        System.out.println(sum);
 
-     
-
-        
-        
 
         //P3: Ask the user for a number, and then add together every OTHER digit (starting from 1)
         System.out.println("Problem 3");
         System.out.println("Enter a number and I will tell you the sum of every other number: ");
+        int userNum2 = sc.nextInt(); 
+        int sum2 = 0;
 
-        String num4 = sc.next(); 
-        int sum3 = 0;
-
-        // Iterate over every other digit starting from index 0
-        for (int i = 0; i < num4.length(); i += 2) { // had to google how to find the length of a string
-            sum3 += Character.getNumericValue(num4.charAt(i)); // had to google how to change a string into an integer, then also found how to breakup string into characters.
+        for (int i = 1; i <= userNum2; i += 2) { 
+            sum2 = sum2 + i; 
         }
 
-        System.out.println(sum3); 
-
-        
+        System.out.println("Sum was: " + sum2); 
 
     
         //No hint! what do you need to complete this task?
-        // Needed to receive number in a string then break it up and convert to integer
+        
 
 
         //P4: Why does this loop never stop!
@@ -62,13 +55,15 @@ public class Main {
 
         //P5: Take a string from the user and print them the reverse!
         System.out.println("Problem 5");
-        System.out.println("Please provide a string");
-        String input = sc.nextLine();
+        System.out.println("Please provide a string: ");
+        String userInp = sc.nextLine();
+        char ch;
         //hint
         String reverse = ""; // this makes the var reverse empty
 
-        for (int i = input.length() - 1; i >= 0; i--) { 
-            reverse += input.charAt(i); // adds the characters backwards into reverse var
+        for (int i = 0 ; i< userInp.length(); i++) { 
+            ch = userInp.charAt(i); 
+            reverse = ch+reverse;
         }
 
         
