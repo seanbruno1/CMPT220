7431d51002bd94792e7061328fe571d09d2b6cf1
seanbruno1 <162635726+seanbruno1@users.noreply.github.com>
2024-10-25 12:18:42 -0400

    done

---
+++ Homework 1/functions.java
@@ -0,0 +1,9 @@
+public class functions {
+    public static void main(String[] args){
+        printName();
+    }
+    
+    public static void printName(){
+        System.out.println("Sean");
+    }
+}
